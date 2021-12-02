471a3b79308f0b9a67747575d86299e5dc7493ad
winest <winestwinest@gmail.com>
2021-12-02 23:18:29 +0800

    [JScript/CWShell] 1. Update code style 2. Change the output of CWUtils.Exec() to get stdout and stderr messages

--- JScript/Windows/CWShell.js
+++ JScript/Windows/CWShell.js
@@ -17,12 +17,12 @@
 
 //Avoid multiple instances
 var CWUtils = CWUtils || {};
-CWUtils.WshShell = CWUtils.WshShell || WScript.CreateObject( "WScript.Shell" );
+CWUtils.WshShell = CWUtils.WshShell || WScript.CreateObject("WScript.Shell");
 
-CWUtils.IsX86 = CWUtils.IsX86 || function()
+CWUtils.IsX86 = CWUtils.IsX86 || function ()
 {
-    var strArch = CWUtils.WshShell.Environment( "System" )("PROCESSOR_ARCHITECTURE").toLowerCase();
-    if ( strArch.match(/x86|i386/) )
+    var strArch = CWUtils.WshShell.Environment("System")("PROCESSOR_ARCHITECTURE").toLowerCase();
+    if (strArch.match(/x86|i386/))
     {
         return true;
     }
@@ -32,56 +32,98 @@ CWUtils.IsX86 = CWUtils.IsX86 || function()
     }
 };
 
-CWUtils.Exec = CWUtils.Exec || function( aCmd , aWaitEnd , aWaitEndOutput , aPrintWhileWaitEnd )
+// Use aMsg.stdout to get value from std::out
+// Use aMsg.stderr to get value from std::err
+CWUtils.Exec = CWUtils.Exec || function (aCmd, aWaitEnd, aMsg, aPrintMsg)
 {
-    aWaitEnd = ( aWaitEnd ) ? true : false;
-    if ( aWaitEndOutput
-         //&& Object.prototype.toString.call( aWaitEndOutput ) === "[object Array]"         //Don't check it so programmer can find bug when developing
-       )
+    aWaitEnd = (aWaitEnd) ? true : false;
+    if (typeof aMsg !== "undefined")
     {
-        aWaitEndOutput.length = 0;
+        aMsg.stdout = "";
+        aMsg.stderr = "";
     }
 
     //Return a WshScriptExec object, refer to http://msdn.microsoft.com/en-us/library/2f38xsxe(v=vs.84).aspx
-    var execObj = CWUtils.WshShell.Exec( aCmd );
-    if ( true == aWaitEnd )
+    var execObj = CWUtils.WshShell.Exec(aCmd);
+    if (true == aWaitEnd)
     {
         var strOutput = "";
-        while ( 0 == execObj.Status )   //0 for running, 1 for completed
+        while (0 == execObj.Status)   //0 for running, 1 for completed
         {
-            WScript.Sleep( 100 );
+            WScript.Sleep(100);
 
             //It's needed to clean StdOut anyway or the execObj.Status will not changed to completed forever
-            while ( false == execObj.StdOut.AtEndOfStream )
+            while (false == execObj.StdOut.AtEndOfStream)
             {
                 strOutput = execObj.StdOut.ReadLine();
-                if ( aPrintWhileWaitEnd )
+                if (aPrintMsg)
                 {
-                    WScript.Echo( strOutput );
+                    WScript.Echo(strOutput);
                 }
-                if ( aWaitEndOutput )
+                if (typeof aMsg !== "undefined")
                 {
-                    aWaitEndOutput.push( strOutput );
+                    if (aMsg.stdout.length > 0)
+                    {
+                        aMsg.stdout += "\n";
+                    }
+                    aMsg.stdout += strOutput;
+                }
+            }
+            //It's needed to clean StdErr anyway or the execObj.Status will not changed to completed forever
+            while (false == execObj.StdErr.AtEndOfStream)
+            {
+                strOutput = execObj.StdErr.ReadLine();
+                if (aPrintMsg)
+                {
+                    WScript.Echo(strOutput);
+                }
+                if (typeof aMsg !== "undefined")
+                {
+                    if (aMsg.stderr.length > 0)
+                    {
+                        aMsg.stderr += "\n";
+                    }
+                    aMsg.stderr += strOutput;
                 }
             }
         }
-        while ( false == execObj.StdOut.AtEndOfStream )
+        while (false == execObj.StdOut.AtEndOfStream)
         {
             strOutput = execObj.StdOut.ReadLine();
-            if ( aPrintWhileWaitEnd )
+            if (aPrintMsg)
             {
-                WScript.Echo( strOutput );
+                WScript.Echo(strOutput);
             }
-            if ( aWaitEndOutput )
+            if (typeof aMsg !== "undefined")
             {
-                aWaitEndOutput.push( strOutput );
+                if (aMsg.stdout.length > 0)
+                {
+                    aMsg.stdout += "\n";
+                }
+                aMsg.stdout += strOutput;
+            }
+        }
+        while (false == execObj.StdErr.AtEndOfStream)
+        {
+            strOutput = execObj.StdErr.ReadLine();
+            if (aPrintMsg)
+            {
+                WScript.Echo(strOutput);
+            }
+            if (typeof aMsg !== "undefined")
+            {
+                if (aMsg.stderr.length > 0)
+                {
+                    aMsg.stderr += "\n";
+                }
+                aMsg.stderr += strOutput;
             }
         }
     }
     return execObj;
 };
 
-CWUtils.RunCmd = CWUtils.RunCmd || function( aCmd , aHideWindow , aWaitEnd )
+CWUtils.RunCmd = CWUtils.RunCmd || function (aCmd, aHideWindow, aWaitEnd)
 {
     // 0: Hides the window and activates another window.
     // 1: Activates and displays a window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when displaying the window for the first time.
@@ -94,48 +136,48 @@ CWUtils.RunCmd = CWUtils.RunCmd || function( aCmd , aHideWindow , aWaitEnd )
     // 8: Displays the window in its current state. The active window remains active.
     // 9: Activates and displays the window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when restoring a minimized window.
     // 10: Sets the show-state based on the state of the program that started the application.
-    var nWindowStyle = ( aHideWindow ) ? 0 : 8;
-    aWaitEnd = ( aWaitEnd ) ? true : false;
-    return CWUtils.WshShell.Run( aCmd , nWindowStyle , aWaitEnd );
+    var nWindowStyle = (aHideWindow) ? 0 : 8;
+    aWaitEnd = (aWaitEnd) ? true : false;
+    return CWUtils.WshShell.Run(aCmd, nWindowStyle, aWaitEnd);
 };
 
-CWUtils.SendKeys = CWUtils.SendKeys || function( aPid , aKeys )
+CWUtils.SendKeys = CWUtils.SendKeys || function (aPid, aKeys)
 {
-    CWUtils.WshShell.AppActivate( aPid );
-    CWUtils.WshShell.SendKeys( aKeys );
+    CWUtils.WshShell.AppActivate(aPid);
+    CWUtils.WshShell.SendKeys(aKeys);
 };
 
-CWUtils.ReadReg = CWUtils.ReadReg || function( aValName )
+CWUtils.ReadReg = CWUtils.ReadReg || function (aValName)
 {
     try
     {
-        return CWUtils.WshShell.RegRead( aValName );
+        return CWUtils.WshShell.RegRead(aValName);
     }
-    catch( err )
+    catch (err)
     {
         return null;
     }
 };
 
-CWUtils.WriteReg = CWUtils.WriteReg || function( aValName , aValContent , aValType )
+CWUtils.WriteReg = CWUtils.WriteReg || function (aValName, aValContent, aValType)
 {
     try
     {
         //REG_SZ, REG_EXPAND_SZ, REG_DWORD, REG_BINARY
         //RegWrite will write at most one DWORD to a REG_BINARY value. Larger values are not supported with this method.
-        CWUtils.WshShell.RegWrite( aValName , aValContent , aValType );
+        CWUtils.WshShell.RegWrite(aValName, aValContent, aValType);
         return true;
     }
-    catch( err )
+    catch (err)
     {
         return false;
     }
 };
 
-CWUtils.DeleteReg = CWUtils.DeleteReg || function( aValName , aIsKey )
+CWUtils.DeleteReg = CWUtils.DeleteReg || function (aValName, aIsKey)
 {
     //If you are deleting a key, you must end the path with back-slash
-    if ( aIsKey && 0 < aValName.length && '\\' != aValName[aValName.length - 1] )
+    if (aIsKey && 0 < aValName.length && '\\' != aValName[aValName.length - 1])
     {
         aValName += "\\";
     }
@@ -143,10 +185,10 @@ CWUtils.DeleteReg = CWUtils.DeleteReg || function( aValName , aIsKey )
     try
     {
 
-        CWUtils.WshShell.RegDelete( aValName );
+        CWUtils.WshShell.RegDelete(aValName);
         return true;
     }
-    catch( err )
+    catch (err)
     {
         return false;
     }
